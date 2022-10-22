99df19a5bc36bc51fb930efb19a589b762a63a63
lildaizee <96581651+lildaizee@users.noreply.github.com>
2022-10-22 16:01:20 +0800

    feat: add tween for main page

--- Assets/Scripts/sceneManager.cs
+++ Assets/Scripts/sceneManager.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class sceneManager : MonoBehaviour
 {
+    //public float delay = 2;
+    //public string newScene = "ARScene";
+
+    //void Start()
+    //{
+    //    StartCoroutine(LoadLevelAfterDelay(delay));
+    //}
+
+    //IEnumerator LoadLevelAfterDelay(float delay)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    SceneManager.LoadScene(newScene);
+    //}
 
     public void changeScene(string value)
     {
         SceneManager.LoadScene(value);
     }
-    
+
 }
